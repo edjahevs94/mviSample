861f9b87ed7dbf392e5409a825ebf4eb599416d3
edjahevs94 <edjahevs@gmail.com>
2024-04-21 15:29:14 -0500

    Initial Commit

---
+++ MVISample/MVISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVISampleApp.swift
+//  MVISample
+//
+//  Created by Edgard Vargas on 21/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MVISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
